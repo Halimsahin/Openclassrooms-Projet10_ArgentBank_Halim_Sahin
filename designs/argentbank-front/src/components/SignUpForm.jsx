944dfe0d17e2f4e1a06a295feb9efef6f131e2c8
Halim Sahin <halimsarazin@gmail.com>
2024-07-26 08:42:21 +0200

    commit 3

--- designs/argentbank-front/src/components/SignUpForm.jsx
+++ designs/argentbank-front/src/components/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 import axios from 'axios'
 
 const SignUpForm = () => {
@@ -8,6 +9,8 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,7 +30,10 @@ const SignUpForm = () => {
         password
       })
 
-      
+      if (response.status === 200) {
+        console.log('Sign up successful:', response.data);
+        navigate('/login');
+      }
 
       console.log('Sign up successful:', response.data)
     } catch (error) {
@@ -37,56 +43,63 @@ const SignUpForm = () => {
   };
 
   return (
+    <section className="sign-in-content">
     <form onSubmit={handleSubmit}>
-      <h2>Sign Up</h2>
+      <h1>Sign Up</h1>
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="username">Firstname:</label>
         <input
           type="text"
           id="fisrtname"
           value={firstname}
           onChange={(e) => setFirstname(e.target.value)}
+          required
         />
       </div>
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="username">Lastname:</label>
         <input
           type="text"
           id="lastname"
           value={lastname}
           onChange={(e) => setLastname(e.target.value)}
+          required
         />
       </div>
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="username">Username:</label>
         <input
           type="text"
           id="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          required
         />
       </div>
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
       </div>
-      <div>
+      <div className="input-wrapper">
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
       </div>
-      <button type="submit">Sign Up</button>
+      <button className="sign-in-button" type="submit">Sign Up</button>
     </form>
+    </section>
   );
 };
 
